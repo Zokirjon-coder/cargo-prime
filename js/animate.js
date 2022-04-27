718f505c4be5f47aa7fd9648cbe90d4048f74e6f
Zokirjon-coder <zokirjonyetmishboyev@gmail.com>
2022-04-27 10:49:22 +0500

    kallasi yo'q eshakni deb dizaynni yana qaytaryaman ortga

--- js/animate.js
+++ js/animate.js
@@ -1,69 +1,69 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   // gsap only onse animation
   // navigation img animation
   const imgTl = gsap.timeline();
 
   imgTl.from(
-    ".gs_flipRight",
+    '.gs_flipRight',
     {
       duration: 0.5,
       x: 30,
       opacity: 0,
       stagger: 0.2,
     },
-    "-=0.3"
+    '-=0.3'
   );
 
   // navigation indicator animation
   const indicatorTl = gsap.timeline();
 
   indicatorTl.from(
-    ".gs_flipLeft",
+    '.gs_flipLeft',
     {
       duration: 0.5,
       x: -30,
       opacity: 0,
       stagger: 0.2,
     },
-    "-=0.3"
+    '-=0.3'
   );
   // header animation
   const headerTl = gsap.timeline();
 
   headerTl.from(
-    ".gs_scalabe",
+    '.gs_scalabe',
     {
       delay: 0.2,
       duration: 0.7,
       scale: 0,
       stagger: 0.2,
     },
-    "-=0.3"
+    '-=0.3'
   );
 
   setTimeout(() => {
-    VanillaTilt.init(document.querySelector(".card"), {
-      max: 15,
-      speed: 400,
-    });
+    // VanillaTilt.init(document.querySelector('.card'), {
+    //   max: 15,
+    //   speed: 400,
+    // });
 
-    //It also supports NodeList
-    VanillaTilt.init(document.querySelectorAll(".card"));
+    // //It also supports NodeList
+    // VanillaTilt.init(document.querySelectorAll('.card'));
 
-    VanillaTilt.init(document.querySelector(".slide_conatiner"), {
-      max: 15,
-      speed: 400,
-    });
+    // VanillaTilt.init(document.querySelector(".slide_conatiner"), {
+    //   max: 15,
+    //   speed: 400,
+    // });
 
-    //It also supports NodeList
-    VanillaTilt.init(document.querySelectorAll(".slide_conatiner"));
+    // //It also supports NodeList
+    // VanillaTilt.init(document.querySelectorAll(".slide_conatiner"));
 
     // for slider
 
-    new Swiper(".mySwiper", {
+    new Swiper('.mySwiper', {
       slidesPerView: 1,
       spaceBetween: 0,
-      slidesPerView: "auto",
+      slidesPerView: 'auto',
       centeredSlides: true,
       grubCursor: true,
       loop: true,
@@ -71,12 +71,12 @@ document.addEventListener("DOMContentLoaded", function () {
       observerParents: true,
 
       pagination: {
-        el: ".swiper-pagination",
+        el: '.swiper-pagination',
         clickable: true,
       },
       navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
       },
     });
 
@@ -84,7 +84,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
     gsap.registerPlugin(ScrollTrigger);
 
-    gsap.utils.toArray(".gs_reveal").forEach(function (elem) {
+    gsap.from('.fon', {
+      scrollTrigger: {
+        trigger: '.fon',
+        toggleActions: 'restart none restart none',
+      },
+      y: 15,
+      height: 0,
+      duration: 0.5,
+      delay: 0.2,
+    });
+
+    gsap.utils.toArray('.gs_reveal').forEach(function (elem) {
       hide(elem); // assure that the element is hidden when scrolled into view
 
       ScrollTrigger.create({
@@ -102,10 +113,10 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     // counter
-    gsap.from(".counting", {
+    gsap.from('.counting', {
       scrollTrigger: {
-        trigger: ".counting",
-        toggleActions: "restart none restart none",
+        trigger: '.counting',
+        toggleActions: 'restart none restart none',
       },
       textContent: 0,
       duration: 2,
@@ -120,28 +131,28 @@ function animateFrom(elem, direction) {
   let x = 0,
     scale = 1,
     y = direction * 100;
-  if (elem.classList.contains("gs_reveal_fromLeft")) {
+  if (elem.classList.contains('gs_reveal_fromLeft')) {
     x = -100;
     y = 0;
-  } else if (elem.classList.contains("gs_reveal_fromRight")) {
+  } else if (elem.classList.contains('gs_reveal_fromRight')) {
     x = 100;
     y = 0;
-  } else if (elem.classList.contains("gs_reveal_fromTop")) {
+  } else if (elem.classList.contains('gs_reveal_fromTop')) {
     x = 0;
     y = -100;
-  } else if (elem.classList.contains("gs_reveal_fromBottom")) {
+  } else if (elem.classList.contains('gs_reveal_fromBottom')) {
     x = 0;
     y = 100;
-  } else if (elem.classList.contains("gs_reveal_fromTransparent")) {
+  } else if (elem.classList.contains('gs_reveal_fromTransparent')) {
     x = 0;
     y = 0;
-  } else if (elem.classList.contains("gs_reveal_fromScale")) {
+  } else if (elem.classList.contains('gs_reveal_fromScale')) {
     x = 0;
     y = 0;
     scale = 0;
   }
-  elem.style.transform = "translate(" + x + "px, " + y + "px)";
-  elem.style.opacity = "0";
+  elem.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+  elem.style.opacity = '0';
   gsap.fromTo(
     elem,
     { x: x, y: y, scale: scale, autoAlpha: 0 },
@@ -152,8 +163,8 @@ function animateFrom(elem, direction) {
       y: 0,
       scale: 1,
       autoAlpha: 1,
-      ease: "expo",
-      overwrite: "auto",
+      ease: 'expo',
+      overwrite: 'auto',
     }
   );
 }
--- js/index.js
+++ js/index.js
@@ -1,53 +1,53 @@
-const searchBtn = document.querySelector(".search_btn");
-const search_place = document.querySelector(".search_place");
-const search_input = document.querySelector(".search_place input");
-const links_place = document.querySelector(".links_place");
-const search_btn = document.querySelector(".search_place img");
-const call_btn = document.querySelector(".links_place .call");
-const btn_place = document.querySelector(".btn_place");
-const links = document.querySelector(".links");
-const search_menu = document.querySelector(".search_menu");
+const searchBtn = document.querySelector('.search_btn');
+const search_place = document.querySelector('.search_place');
+const search_input = document.querySelector('.search_place input');
+const links_place = document.querySelector('.links_place');
+const search_btn = document.querySelector('.search_place img');
+const call_btn = document.querySelector('.links_place .call');
+const btn_place = document.querySelector('.btn_place');
+const links = document.querySelector('.links');
+const search_menu = document.querySelector('.search_menu');
 
-search_input.addEventListener("input", (e) => {
+search_input.addEventListener('input', (e) => {
   if (e.target.value.length > 0)
-    search_place.querySelector("img").classList.remove("hidden");
-  else if (!search_place.querySelector("img").classList.contains("hidden"))
-    search_place.querySelector("img").classList.add("hidden");
+    search_place.querySelector('img').classList.remove('hidden');
+  else if (!search_place.querySelector('img').classList.contains('hidden'))
+    search_place.querySelector('img').classList.add('hidden');
 });
 
-search_menu.addEventListener("input", (e) => {
+search_menu.addEventListener('input', (e) => {
   if (e.target.value.length > 0)
-    search_menu.querySelector("img").classList.remove("hidden");
-  else if (!search_menu.querySelector("img").classList.contains("hidden"))
-    search_menu.querySelector("img").classList.add("hidden");
+    search_menu.querySelector('img').classList.remove('hidden');
+  else if (!search_menu.querySelector('img').classList.contains('hidden'))
+    search_menu.querySelector('img').classList.add('hidden');
 });
 
 function setHidden(element) {
-  element.classList.contains("hidden")
-    ? element.classList.remove("hidden")
-    : element.classList.add("hidden");
+  element.classList.contains('hidden')
+    ? element.classList.remove('hidden')
+    : element.classList.add('hidden');
 }
 
 function focusInput(element) {
-  if (!element.parentElement.classList.contains("hidden")) element.focus();
+  if (!element.parentElement.classList.contains('hidden')) element.focus();
 }
 
-searchBtn.addEventListener("click", () => {
+searchBtn.addEventListener('click', () => {
   setHidden(search_place);
   setHidden(links_place);
   focusInput(search_input);
-  searchBtn.classList.toggle("search_show");
-  btn_place.classList.toggle("search_show");
-  if (btn_place.classList.contains("search_show")) {
+  searchBtn.classList.toggle('search_show');
+  btn_place.classList.toggle('search_show');
+  if (btn_place.classList.contains('search_show')) {
     btn_place.scrollTop = 100;
   } else {
     btn_place.scrollTop = 0;
   }
 });
 
-call_btn.addEventListener("click", () => {
-  call_btn.classList.toggle("search_show");
-  if (call_btn.classList.contains("search_show")) {
+call_btn.addEventListener('click', () => {
+  call_btn.classList.toggle('search_show');
+  if (call_btn.classList.contains('search_show')) {
     links.scrollTop = 100;
   } else {
     links.scrollTop = 0;
@@ -56,61 +56,220 @@ call_btn.addEventListener("click", () => {
 
 // on scroll
 
-const wrapper = document.querySelector(".wrapper");
-const sections = document.querySelectorAll(".wrapper > section");
-const navbtns = document.querySelectorAll(".navigation .nav_btn");
+const wrapper = document.querySelector('.wrapper');
+const sections = document.querySelectorAll('.wrapper > section');
+const navbtns = document.querySelectorAll('.navigation .nav_btn');
 
-document.addEventListener("scroll", (e) => {
+document.addEventListener('scroll', (e) => {
   const scrollY = e.srcElement.scrollingElement.scrollTop;
 
   sections.forEach((section) => {
     const sectionHeight = section.offsetHeight;
     const sectionTop = section.offsetTop - 200;
     if (scrollY > sectionTop && scrollY < sectionTop + sectionHeight) {
-      const sectionId = section.getAttribute("id");
+      const sectionId = section.getAttribute('id');
       navbtns.forEach((btn) => {
-        if (btn.getAttribute("title") === sectionId) {
-          navbtns.forEach((button) => button.classList.remove("active"));
-          btn.classList.add("active");
+        if (btn.getAttribute('data-title') === sectionId) {
+          navbtns.forEach((button) => button.classList.remove('active'));
+          btn.classList.add('active');
         }
       });
     }
   });
 });
 
-// for map
-
-ymaps.ready(init);
-function init() {
-  var myMap = new ymaps.Map("map", {
-    center: [55.76, 37.64],
-    zoom: 7,
-  });
-}
-
 //  floating menu settings
-const float_menu = document.querySelector(".float_menu");
-const hamburger_btn = document.querySelector(".hamburger_btn");
-const menu_close = document.querySelector(".menu_close");
-const float_menus = float_menu.querySelectorAll(".menu_item");
+const float_menu = document.querySelector('.float_menu');
+const hamburger_btn = document.querySelector('.hamburger_btn');
+const menu_close = document.querySelector('.menu_close');
+const float_menus = float_menu.querySelectorAll('.menu_item');
 
-hamburger_btn.addEventListener("click", () => {
-  float_menu.classList.add("opened");
+hamburger_btn.addEventListener('click', () => {
+  float_menu.classList.add('opened');
 });
 
-float_menu.addEventListener("click", (e) => {
+float_menu.addEventListener('click', (e) => {
   e.stopPropagation();
 });
 
 const closeMenu = () => {
-  float_menu.classList.remove("opened");
+  float_menu.classList.remove('opened');
 };
 
-menu_close.addEventListener("click", (e) => {
+menu_close.addEventListener('click', (e) => {
   e.stopPropagation();
   closeMenu();
 });
 
 float_menus.forEach((menu) => {
-  menu.addEventListener("click", closeMenu);
+  menu.addEventListener('click', closeMenu);
+});
+
+// slider trucks
+const prevBtn = document.querySelector('.slidenavBtns .prev');
+const nextBtn = document.querySelector('.slidenavBtns .next');
+const slide = document.querySelector('.slider_container .images');
+const params = document.querySelector('.slider_container .params');
+const numberSlide = document.querySelector('.numberSlide .number');
+const numberLenght = document.querySelector('.numberSlide .length');
+
+// images
+// const img1 = './assets/images/img1.png';
+// const img2 = './assets/images/img2.png';
+// const img3 = './assets/images/img3.png';
+
+const dataSlider = [
+  {
+    id: 1,
+    // image: img1,
+    param: [
+      { id: 1, param: 'Governed at 70 mph' },
+      { id: 2, param: 'Inverter' },
+      { id: 3, param: 'Automatic' },
+      { id: 4, param: 'Combo sleeper' },
+    ],
+  },
+  {
+    id: 2,
+    // image: img2,
+    param: [
+      { id: 1, param: 'Governed at 70 mph' },
+      { id: 2, param: 'Inverter' },
+      { id: 3, param: 'Automatic' },
+      { id: 4, param: 'Combo sleeper' },
+    ],
+  },
+  {
+    id: 3,
+    // image: img3,
+    param: [
+      { id: 1, param: 'Governed at 70 mph' },
+      { id: 2, param: 'Inverter' },
+      { id: 3, param: 'Automatic' },
+      { id: 4, param: 'Combo sleeper' },
+    ],
+  },
+];
+
+function setImageSlide(data) {
+  const images = slide.querySelectorAll('img');
+  const { id, param } = data;
+  const paragraphs = params.querySelectorAll('p');
+  const timeLine = gsap.timeline();
+  let img = null;
+  images.forEach((image) => {
+    let disabled = image.getAttribute('disabled');
+    if (disabled === 'false') {
+      img = image;
+    }
+  });
+
+  numberSlide.innerHTML = String(id);
+  gsap.fromTo(
+    img,
+    {
+      x: 0,
+      opacity: 1,
+    },
+    {
+      opacity: 0,
+      x: -100,
+      duration: 0.5,
+    }
+  );
+  timeLine.fromTo(
+    paragraphs,
+    {
+      x: 0,
+      opacity: 1,
+    },
+    {
+      opacity: 0,
+      x: 100,
+      stagger: 0.1,
+      duration: 0.5,
+    },
+    '-=.2'
+  );
+
+  setTimeout(() => {
+    paragraphs.forEach((paragraph) => (paragraph.innerHTML = ''));
+    param.forEach(({ param }, index) => {
+      paragraphs[index].innerHTML += param;
+    });
+    images.forEach((image, index) => {
+      if (id === index + 1) {
+        image.setAttribute('disabled', 'false');
+        img = image;
+      } else image.setAttribute('disabled', 'true');
+    });
+
+    gsap.fromTo(
+      img,
+      {
+        x: 100,
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 0.5,
+      }
+    );
+    timeLine.fromTo(
+      paragraphs,
+      {
+        x: -50,
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        stagger: 0.1,
+        duration: 0.5,
+      },
+      '-=.2'
+    );
+  }, 900);
+}
+
+const slideCheck = (index) => {
+  const lastIndex = dataSlider.length - 1;
+  if (index < 0) {
+    return lastIndex;
+  }
+  if (index > lastIndex) {
+    return 0;
+  }
+  return index;
+};
+
+let n = 0;
+
+setImageSlide(dataSlider[n]);
+
+const setSlide = () => {
+  n = slideCheck(++n);
+  setImageSlide(dataSlider[n]);
+};
+
+const intervalSlide = setInterval(setSlide, 10000);
+
+const onChangeSlide = (change) => {
+  if (change === 'next') {
+    n++;
+  } else {
+    n--;
+  }
+  n = slideCheck(n);
+  setImageSlide(dataSlider[n]);
+};
+
+prevBtn.addEventListener('click', () => {
+  onChangeSlide('prev');
+  clearInterval(intervalSlide);
+});
+nextBtn.addEventListener('click', () => {
+  onChangeSlide('next');
+  clearInterval(intervalSlide);
 });
